6169de59ad1ea1c894b7734ee58721d5f435bb3b
Jesse Phillips <Jesse.K.Phillips@gmail.com>
2012-09-24 21:39:37 -0700

    Publish

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
 listings-dlang-extractor
 ========================
 
-Extracts and runs source code from Latex listings
+This program will extract programs from lstlisting in tex files.
+
+This program is built specifically to compile D programs in lstlisting for
+the book "Learning with D." There is no intention to generalize or make customizable.
+
+The goal is to include the results of a compile and run into the tex file.
+
+Usage
+-----
+
+Reads lex file from standard in, writes to standard out.
+
+Latex Listings
+--------------
+
+Insert code between the standard \begin{lstlisting} and \end{lstlisting} tags. Simple as that... with these complications.
+
+Captions can be added, but must be on the same line as the begin. Other options can be included, but remain on the same line unless you wish to fix the code.
+
+    \begin{lstlisting}[caption={Standard Input}]
+
+Input can be entered, but there will be no output to explain what is being requested. Instead add Input: comments to your code to allow automation of inputing commands. Any string which requests input should use the &lt;input&gt; so that program run is correctly reflected in the output latex file.
+
+    // Input: Jesse
+    write("Please enter your name: <input>");
+
+There is no intention to support any graphical programs, I may provide a signal to ignore a listing.
+
+Multiple Files
+--------------
+
+Support for multiple files does exist. Just place multiple listings together without any explanation text between them. To import an example the example must be named. Provide a module declaration will allow importing via that name. As files are never removed, these named modules can be imported in subsequent examples, or even overwritten.
+
+Unit Testing
+------------
+
+D provides in built unittest blocks. These are only run when passed the -unittest argument to the compiler. Simply add unittest blocks to your code, the compiler will be told to compile them in.
+
+Program Arguments
+-----------------
+
+Command line arguments are not supported at this time, but I'll likely need them in the future.
+
+Compiler Arguments
+------------------
+
+There is no option to pass arguments to the compiler. This will likely need changed in the future.
+
+I will also likely need a way to specify multiple compile/runs against the same examples. This would help explain version/debug blocks.
+
+Currently unittest is the only supported option and is automatically detected.
---
+++ extract-listings.d
@@ -0,0 +1,287 @@
+/**
+ * This program will extract programs from lstlisting in tex files.
+ *
+ * This program is built specifically to compile D programs in lstlisting for
+ * the book "Learning with D."
+ *
+ * The goal is to include the results of a compile and run into the tex file.
+ */
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.exception;
+import std.file;
+import std.path;
+import std.process;
+import std.range;
+import std.regex;
+import std.stdio;
+import std.string;
+
+int exampleCount;
+
+void main(string[] args) {
+    //enforce(args.length == 2 || args.length == 3,
+    //        "Usage: " ~ args[0] ~ " file [example number]");
+    int compileOnly;
+    if(args.length == 2)
+        compileOnly = to!int(args[1]);
+
+    string file;
+    foreach(line; stdin.byChunk(4096))
+        file ~= line;
+    while(true) {
+        auto arr = file.findSplit(r"\begin{lstlisting}");
+        write(arr[0]);
+        file = arr[2];
+
+        if(file.empty)
+            break;
+        string[] proglist;
+
+        arr = file.findSplit(r"\end{lstlisting}");
+        outputProgram(arr[0]);
+        arr[0].findSkip("]");
+        proglist ~= writeProgram(arr[0].strip);
+
+        file = arr[2].strip;
+        auto progtxt = arr[0];
+        while(file.startsWith(r"\begin{lstlisting}")) {
+            file.skipOver(r"\begin{lstlisting}");
+            arr = file.findSplit(r"\end{lstlisting}");
+            outputProgram(arr[0]);
+            arr[0].findSkip("]");
+            proglist ~= writeProgram(arr[0].strip);
+            progtxt ~= arr[0];
+            file = arr[2].strip;
+        }
+
+        if(compileOnly)
+            if(compileOnly != exampleCount)
+                continue;
+
+        foreach(i; importFiles(progtxt))
+            if(proglist.find(i).empty)
+                proglist ~= i;
+
+        executeExample(proglist, progtxt);
+
+        writeln(readText(format("example%s.compile", exampleCount)));
+    }
+}
+
+auto executeExample(string[] proglist, string progtxt) {
+        auto f = File(format("example%s.compile", exampleCount), "w");
+        scope(exit) f.close();
+        f.writeln(r"\begin{verbatim}");
+        scope(exit) f.writeln(r"\end{verbatim}");
+        auto options = compileOptions(progtxt);
+        auto inputs = inputsForFile(progtxt);
+        if(compile(proglist, options, f))
+            runProgram(proglist, inputs, f);
+}
+
+auto compileOptions(R)(R progtxt) {
+    string cmd;
+    if(!match(progtxt, regex("unittest")).empty)
+        cmd ~= "-unittest ";
+    return cmd;
+}
+
+auto compile(string[] proglist, string options, File f) {
+    string cmd = "dmd " ~ options;
+    string compilerOutput = "compiler.out";
+
+    auto make = cmd ~ proglist.join(" ");
+    debug writeln(make);
+    f.writeln("$ " ~ make);
+    auto ret = system(make ~ " > " ~ compilerOutput ~ " 2>&1");
+    auto compilerText = readText(compilerOutput);
+    scope(exit) remove(compilerOutput);
+
+    debug if(compilerText) writeln(compilerText);
+    if(!compilerText.empty)
+        breakLines(compilerText, f);
+    // No need to continue if compilation failed
+    if(ret != 0) {
+        debug {
+            writeln("Compile Failed");
+            writeln(make ~ " > " ~ compilerOutput ~ " 2>&1");
+            writeln();
+        }
+        return false;
+    }
+
+    return true;
+}
+
+enum inputReg = ctRegex!("<input>");
+auto runProgram(string[] proglist, string[] inputs, File f) {
+    string programOutput = "program.out";
+    string inputFile = "program.in";
+    auto run = "./" ~ stripExtension(proglist.front);
+    if(!inputs.empty) {
+        auto inFile = File(inputFile, "w");
+        scope(exit) if(exists(inputFile)) remove(inputFile);
+        foreach(input; inputs)
+            inFile.writeln(input);
+        debug writeln(
+               format(run ~ "< %s 2>&1 | tee %s", inputFile, programOutput));
+        system(format(run ~ "< %s > %s 2>&1", inputFile, programOutput));
+    } else {
+        debug writeln(run ~ " 2>&1 | tee " ~ programOutput);
+        system(run ~ " > " ~ programOutput ~ " 2>&1");
+    }
+    scope(exit) remove(programOutput);
+    auto outText = readText(programOutput);
+
+    foreach(input; inputs) {
+        outText = outText.replace(inputReg, input ~ "\n");
+    }
+
+    debug if(progText) writeln(outText);
+    f.writeln("$ " ~ run);
+    if(!outText.empty)
+        breakLines(outText.strip(), f);
+}
+
+auto writeProgram(R)(R prog) {
+    auto name = programName(prog);
+
+    auto f = File(name, "w");
+    scope(exit) f.close();
+    f.writeln(prog);
+
+    return name;
+}
+
+auto breakLines(string text, File f) {
+    auto txt = to!(dstring)(text);
+    while(!txt.empty) {
+        if(txt.length <= 55) {
+            if(txt.back == '\n')
+                f.write(txt);
+            else
+                f.writeln(txt);
+            break;
+        }
+
+        auto lineLength = txt.countUntil("\n");
+        if(lineLength != -1 && lineLength <= 55) {
+            f.writeln(txt[0..lineLength]);
+            txt = txt[lineLength+1..$];
+            continue;
+        }
+
+        auto fade = txt[46..56].retro.countUntil(" ");
+        if(fade == -1) {
+            f.writeln(txt[0..56]);
+            txt = txt[56..$];
+            continue;
+        }
+
+        assert(txt[56-fade-1] == ' ');
+        f.writeln(txt[0..56-fade-1]);
+        txt = txt[56-fade..$];
+    }
+}
+
+auto programName(R)(R prog) {
+    exampleCount++;
+    auto name = format("example%s.d", exampleCount);
+    auto m = match(prog, regex("module (.+);"));
+    if(!m.empty) {
+        name = format("example_%s.d", m.captures[1]);
+    }
+
+    return name;
+}
+
+unittest {
+    scope(success) exampleCount = 0;
+    exampleCount = 0;
+    assert("example1.d" == programName(q{
+    import std.stdio;
+    writeln("Hello");
+    }.strip()));
+
+    assert("example2.d" == programName(q{
+    import std.stdio;
+    writeln("Hello");
+    }.strip()));
+
+    assert("example_hello.d" == programName(q{
+    module hello;
+    import std.stdio;
+    writeln("Hello");
+    }.strip()));
+}
+
+auto importFiles(R)(R prog) {
+    string[] proglist;
+    foreach(line; prog.splitLines()) {
+        line = line.strip();
+        if(skipOver(line, "import ")) {
+            line.replace(r"\.", "/");
+            line = to!string(array(line.until(" ")));
+            line = to!string(array(line.until(":")));
+            line = to!string(array(line.until(";")));
+            if(exists("example_"~line~".d"))
+                proglist ~= "example_"~line~".d";
+        } else {
+            // Probably can ignore for sake of std.
+            //TODO: Can't find file
+        }
+    }
+
+    return proglist;
+}
+
+unittest {
+    assert([] == importFiles(q{
+    module hello;
+    import std.stdio;
+    }.strip()));
+
+    system("touch example_triangle.d");
+    scope(exit) std.file.remove("example_triangle.d");
+    assert(["example_triangle.d"] == importFiles(q{
+    module hello;
+    import triangle;
+    import std.stdio;
+    }.strip()));
+}
+
+auto inputsForFile(R)(R prog) {
+    string[] inputs;
+    foreach(line; prog.splitLines()) {
+        line = line.strip();
+        if(skipOver(line, "// Input: ")) {
+            inputs ~= line;
+        }
+    }
+
+    return inputs;
+}
+
+unittest {
+    assert(["Jesse", "24"] == inputsForFile(q{
+    module hello;
+    import std.stdio;
+    // Input: Jesse
+    // Input: 24
+    }.strip()));
+}
+
+auto outputProgram(R)(R progtxt) {
+    write(r"\begin{lstlisting}");
+    foreach(line; progtxt.splitLines()) {
+        if(match(line.strip(), "// Input: ")) {
+            continue;
+        }
+        line = line.replace(inputReg, "");
+        writeln(line);
+    }
+    writeln(r"\end{lstlisting}");
+    writeln();
+}
